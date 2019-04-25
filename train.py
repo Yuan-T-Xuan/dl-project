1fa5d0024cbeb1be5b0cd47941aab139edd4dde4
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2019-04-24 21:59:02 -0400

    modify train.py to work on CUDA

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import torch
 import model
+from tqdm import tqdm
 from torch import optim
 
 def loss_D(real_imgs, generated_imgs, dis):
@@ -10,9 +11,9 @@ def loss_D(real_imgs, generated_imgs, dis):
 def loss_G(generated_imgs, dis):
     return torch.mean((dis(generated_imgs) - 1.0).pow(2))
 
-def loss_cyc(imgs_x, imgs_y, G, F):
-    part1 = torch.mean((F(G(imgs_x)) - imgs_x).pow(2))
-    part2 = torch.mean((G(F(imgs_y)) - imgs_y).pow(2))
+def loss_cyc(generated_x, generated_y, imgs_x, imgs_y, G, F):
+    part1 = torch.mean((F(generated_y) - imgs_x).pow(2))
+    part2 = torch.mean((G(generated_x) - imgs_y).pow(2))
     return part1 + part2
 
 def train(G, F, Dx, Dy, lr, data_loader_x, data_loader_y, epochs, lmbda):
@@ -23,24 +24,25 @@ def train(G, F, Dx, Dy, lr, data_loader_x, data_loader_y, epochs, lmbda):
     for e in range(epochs):
         print(e)
         _iter_y = data_loader_y.__iter__()
-        for _imgs_x, _ in data_loader_x:
+        for _imgs_x, _ in tqdm(data_loader_x):
             _imgs_y = _iter_y.__next__()[0]
+            _imgs_x = _imgs_x.cuda()
+            _imgs_y = _imgs_y.cuda()
             generated_x = F(_imgs_y)
             generated_y = G(_imgs_x)
             # train Ds
             Dx.zero_grad()
             loss_x = loss_D(_imgs_x, generated_x, Dx)
-            loss_x.backward()
+            loss_x.backward(retain_graph=True)
             optimizer_Dx.step()
             Dy.zero_grad()
             loss_y = loss_D(_imgs_y, generated_y, Dy)
-            loss_y.backward()
+            loss_y.backward(retain_graph=True)
             optimizer_Dy.step()
             # train G and F
             G.zero_grad()
             F.zero_grad()
-            loss = loss_G(generated_x, Dx) + loss_G(generated_y, Dy) + lmbda * loss_cyc(_imgs_x, _imgs_y, G, F)
-            loss.backward()
+            loss = loss_G(generated_x, Dx) + loss_G(generated_y, Dy) + lmbda * loss_cyc(generated_x, generated_y, _imgs_x, _imgs_y, G, F)
+            loss.backward(retain_graph=False)
             optimizer_G.step()
             optimizer_F.step()
-
