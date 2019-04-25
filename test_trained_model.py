70651a77c2c28ee9d6483a5336cb1f15d644ee0d
Yuan-T-Xuan <tonyxuan26@foxmail.com>
2019-04-25 00:07:53 -0400

    add test_trained_model.py

---
+++ test_trained_model.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from data_loader import *
+from model import *
+
+dl_manga = get_dataloader(path="/Users/xuan/Downloads/manga_faces", batch_size=4)
+dl_photo = get_dataloader(path="/Users/xuan/photo_faces", batch_size=4)
+
+g = torch.load("/Users/xuan/Downloads/saved_G_3.pt", map_location='cpu')
+f = torch.load("/Users/xuan/Downloads/saved_F_3.pt", map_location='cpu')
+
+manga_sample_y = dl_manga.__iter__().__next__()[0]
+photo_sample_x = dl_photo.__iter__().__next__()[0]
+generated_y = g(photo_sample_x)
+generated_x = f(manga_sample_y)
+
+for i in range(4):
+    plt.imshow(photo_sample_x[i].permute(1, 2, 0).detach().numpy() * 0.5 + 0.5)
+    plt.show()
+    plt.imshow(generated_y[i].permute(1, 2, 0).detach().numpy() * 0.5 + 0.5)
+    plt.show()
+
+for i in range(4):
+    plt.imshow(manga_sample_y[i].permute(1, 2, 0).detach().numpy() * 0.5 + 0.5)
+    plt.show()
+    plt.imshow(generated_x[i].permute(1, 2, 0).detach().numpy() * 0.5 + 0.5)
+    plt.show()
+
